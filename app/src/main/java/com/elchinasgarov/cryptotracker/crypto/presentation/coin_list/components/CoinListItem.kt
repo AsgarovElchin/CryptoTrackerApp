228e2f151d382e99e8852d4141cb8ad171c8354c
Elchin <elchin.asgarov@azerimed.com>
2024-10-20 19:01:39 +0400

    Add Coin.toCoinUI function and Double.toDisplayableNumber function

---
+++ app/src/main/java/com/elchinasgarov/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
@@ -0,0 +1,6 @@
+package com.elchinasgarov.cryptotracker.crypto.presentation.coin_list.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CoinListItem() {}
--- app/src/main/java/com/elchinasgarov/cryptotracker/crypto/presentation/models/CoinUI.kt
+++ app/src/main/java/com/elchinasgarov/cryptotracker/crypto/presentation/models/CoinUI.kt
@@ -1,6 +1,10 @@
 package com.elchinasgarov.cryptotracker.crypto.presentation.models
 
+import android.icu.text.NumberFormat
 import androidx.annotation.DrawableRes
+import com.elchinasgarov.cryptotracker.crypto.domain.Coin
+import com.elchinasgarov.cryptotracker.util.getDrawableIdForCoin
+import java.util.Locale
 
 data class CoinUI(
     val id: String,
@@ -18,3 +22,27 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+fun Coin.toCoinUI(): CoinUI {
+    return CoinUI(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
