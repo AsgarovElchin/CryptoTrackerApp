f1609cec74686dbf7938046df527a19515f6a8ad
AsgarovElchin <elcinaskerovki@gmail.com>
2024-10-21 13:06:36 +0400

    Complete CoinDataSource and RemoteCoinDataSource implementation with DTO mapping

--- app/src/main/java/com/elchinasgarov/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/elchinasgarov/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -1,8 +1,12 @@
 package com.elchinasgarov.cryptotracker.crypto.data.networking
 
+import com.elchinasgarov.cryptotracker.core.data.networking.constructURL
 import com.elchinasgarov.cryptotracker.core.data.networking.safeCall
 import com.elchinasgarov.cryptotracker.core.domain.util.NetworkError
 import com.elchinasgarov.cryptotracker.core.domain.util.Result
+import com.elchinasgarov.cryptotracker.core.domain.util.map
+import com.elchinasgarov.cryptotracker.crypto.data.mappers.toCoin
+import com.elchinasgarov.cryptotracker.crypto.data.networking.dto.CoinResponseDto
 import com.elchinasgarov.cryptotracker.crypto.domain.Coin
 import com.elchinasgarov.cryptotracker.crypto.domain.CoinDataSource
 import io.ktor.client.HttpClient
@@ -10,10 +14,14 @@ import io.ktor.client.request.get
 
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
-):CoinDataSource {
+) : CoinDataSource {
     override suspend fun getCoins(): Result<List<Coin>, NetworkError> {
-        return safeCall {
-            httpClient.get()
+        return safeCall<CoinResponseDto> {
+            httpClient.get(
+                urlString = constructURL("/assets")
+            )
+        }.map { response ->
+            response.data.map { it.toCoin() }
         }
     }
 }
